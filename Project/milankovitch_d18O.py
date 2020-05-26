5a050aa79a281b430c8e8d531bf547269836c9f3
Michael McLean <michael101467@gmail.com>
2020-05-25 22:41:59 -0400

    Code cleaning

--- Project/milankovitch_d18O.py
+++ Project/milankovitch_d18O.py
@@ -3,6 +3,7 @@ import matplotlib as mpl
 import matplotlib.font_manager as font_manager
 from matplotlib import pyplot as plt
 from scipy import signal
+
 EPS = 10**(-5)
 mpl.rcParams['font.family']='serif'
 cmfont = font_manager.FontProperties(fname=mpl.get_data_path()\
@@ -13,8 +14,7 @@ mpl.rcParams['mathtext.fontset']='cm'
 mpl.rcParams['axes.unicode_minus']=False
 
 # define plotting subroutine
-def makePlot(filename,x,y,xlab,ylab,title,legend=None,xlim=None,\
-             ylim=None,figsize=(14,6),semilog=False,annotations=None):
+def makePlot(filename,x,y,xlab,ylab,title,legend=None,xlim=None,ylim=None,figsize=(14,6),semilog=False,annotations=None):
     fig = plt.figure(figsize=figsize)
     if not semilog:
         plt.plot(x,y)
@@ -39,8 +39,8 @@ def makePlot(filename,x,y,xlab,ylab,title,legend=None,xlim=None,\
 
 # define bandpass filtering subroutine
 def butterworthFilter(x, lowf, highf, fs, order=5):
-    low = lowf/(0.5*fs)
-    high = highf/(0.5*fs)
+    low = lowf / (0.5*fs)
+    high = highf / (0.5*fs)
     b,a = signal.butter(order, [low, high], btype='band')
     return signal.lfilter(b, a, x)
 
@@ -53,11 +53,12 @@ sterr = temp[:,2]
 # subsample at period of 5 ka
 times_sub = []
 d18O_sub = []
+
 for i,time in enumerate(times):
     # note that due to the variation in sampling frequency, between
     # indices of 600 and 1051, the signal was sampled at increment of 2 ka,
     # so we must average adjacent signal values in this range.
-    if (time % 5.000 == 0.0): # all other sampling increments divide 5 ka evenly.
+    if (time % 5.000 < EPS): # all other sampling increments divide 5 ka evenly.
         times_sub.append(time)
         d18O_sub.append(d18O[i])
     elif ((int(time) % 10 == 4) and (i > 600) and (i < 1051)):
@@ -69,6 +70,7 @@ d18O_sub = np.asarray(d18O_sub)
 
 # compute temperature
 temp = 16.5 - 4.3*d18O_sub + 0.14*(d18O_sub**2)
+
 # plot raw data
 makePlot("temp_5ka_samp",times_sub,temp,"Time (ka)","Temperature (ºC)",\
          "Proxy for Average Deep Ocean Temperature over time, using the LR04"+\
@@ -79,11 +81,13 @@ makePlot("temp_5ka_samp",times_sub,temp,"Time (ka)","Temperature (ºC)",\
 p = np.polyfit(times_sub, temp, 1)
 trend = np.polyval(p, times_sub)
 temp_detrended = temp - trend
+
 # filter data
 lowcut = 1./500.
 highcut = 1./10.005
 fs = 1/dt
 temp_filt = butterworthFilter(temp_detrended, lowcut, highcut, fs)
+
 # plot filtered and detrended temperature
 makePlot("temp_filtered_5ka_samp",times_sub,temp_filt,"Time (ka)","Detrended "+\
          "Temperature ($\Delta$ºC)",\
@@ -95,6 +99,7 @@ makePlot("temp_filtered_5ka_samp",times_sub,temp_filt,"Time (ka)","Detrended "+\
 # compute DFT of filtered and detrended temperature
 temp_filt_f_sh = np.fft.fftshift(np.fft.fft(temp_filt*dt))
 freq_ax = np.fft.fftshift(np.fft.fftfreq(len(temp_filt_f_sh), dt))
+
 # plot DFT amplitude of filtered and detrended temperature
 makePlot("temp_fourier_5ka_samp_amp",freq_ax,np.abs(temp_filt_f_sh),\
          "Frequency ($ka^{-1}$)","Amplitude",\
@@ -106,6 +111,7 @@ makePlot("temp_fourier_5ka_samp_amp",freq_ax,np.abs(temp_filt_f_sh),\
 # define convolution window
 N = 25
 hann = np.hanning(N)
+
 # plot convolved DFT amplitude spectra
 makePlot("temp_fourier_5ka_samp_amp_conv",freq_ax,\
         np.convolve(hann,np.abs(temp_filt_f_sh),'same'),\
